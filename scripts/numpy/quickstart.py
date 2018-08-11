06644348bb666ea2a20a6c064aff463e06310a53
zhongchun <zhongchunyu@gmail.com>
2018-08-11 23:46:06 +0800

    add some numpy basic use.

--- scripts/numpy/quickstart.py
+++ scripts/numpy/quickstart.py
@@ -96,3 +96,103 @@ print('=' * 60)
 print(f)
 
 # Printing Arrays
+"""
+When you print an array, NumPy displays it in a similar way to nested lists, but with the following layout:
+    the last axis is printed from left to right,
+    the second-to-last is printed from top to bottom,
+    the rest are also printed from top to bottom, with each slice separated from the next by an empty line.
+"""
+a = np.arange(6)
+print('=' * 60)
+print(a)
+
+b = np.arange(12).reshape(4, 3)
+print('=' * 60)
+print(b)
+
+c = np.arange(24).reshape(2, 3, 4)
+print('=' * 60)
+print(c)
+
+print('=' * 60)
+print(np.arange(10000))
+
+print('=' * 60)
+# force NumPy to print the entire array
+# np.set_printoptions(threshold=np.nan)
+print(np.arange(10000).reshape(100, 100))
+
+# Basic Operations
+# Arithmetic operators in arrays apply elementwise
+a = np.array([20, 30, 40, 50])
+b = np.arange(4)
+print('=' * 60)
+print(a)
+print(b)
+c = a - b
+print('=' * 60)
+print(c)
+d = b**2
+print('=' * 60)
+print(d)
+e = 10 * np.sin(a)
+print('=' * 60)
+print(e)
+f = a < 35
+print('=' * 60)
+print(f)
+
+A = np.array([[1, 1], [0, 1]])
+B = np.array([[2, 0], [3, 4]])
+C = A * B
+print('=' * 60)
+print(C)
+D = A @ B
+print('=' * 60)
+print(D)
+E = A.dot(B)
+print('=' * 60)
+print(E)
+
+a = np.ones(3, dtype=np.int32)
+b = np.linspace(0, pi, 3)
+print('=' * 60)
+print(a.dtype)
+print(b.dtype.name)
+c = a + b
+print('=' * 60)
+print(c.dtype)
+print(c)
+d = np.exp(c * 1j)
+print('=' * 60)
+print(d.dtype)
+print(d)
+
+a = np.random.random((2, 3))
+print('=' * 60)
+print(a)
+print('a.sum =', a.sum())
+print('a.min =', a.min())
+print('a.max =', a.max())
+
+b = np.arange(12).reshape(3, 4)
+print('=' * 60)
+print(b)
+print('=' * 60)
+print(b.sum(axis=0))
+print('=' * 60)
+print(b.min(axis=0))
+print('=' * 60)
+print(b.cumsum(axis=1))
+
+# Universal Functions, i.e. ufunc
+B = np.arange(3)
+print('=' * 60)
+print(B)
+print('=' * 60)
+print(np.exp(B))
+print('=' * 60)
+print(np.sqrt(B))
+C = np.array([2., -1., 4.])
+print('=' * 60)
+print(np.add(B, C))
