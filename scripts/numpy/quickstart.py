2ca577924e124aaa1e588093a81114d3ef13e2ef
zhongchun <zhongchunyu@gmail.com>
2018-08-14 22:30:29 +0800

    add a new basic use of numpy

--- scripts/numpy/quickstart.py
+++ scripts/numpy/quickstart.py
@@ -12,6 +12,7 @@
 import numpy as np
 from numpy import pi
 from numpy import newaxis
+import matplotlib.pyplot as plt
 
 # The Basics
 """
@@ -385,3 +386,74 @@ print(a[i])
 j = np.array([[3, 4], [9, 7]])
 print('=' * 60)
 print(a[j])
+
+# When the indexed array a is multidimensional,a single
+# array of indices refers to the first dimension of a.
+palette = np.array([
+    [0, 0, 0],  # black
+    [255, 0, 0],  # red
+    [0, 255, 0],  # green
+    [0, 0, 255],  # blue
+    [255, 255, 255]  # white
+])
+image = np.array([[0, 1, 2, 0], [0, 3, 4, 0]])
+print('=' * 60)
+print(palette[image])
+
+a = np.arange(12).reshape(3, 4)
+print('=' * 60)
+print(a)
+i = np.array([[0, 1], [1, 2]])
+j = np.array([[2, 1], [3, 3]])
+print('=' * 60)
+print(a[i, j])
+
+# Indexing with Boolean Arrays
+# With boolean indices the approach is different; we explicitly
+# choose which items in the array we want and which ones we don’t.
+a = np.arange(12).reshape(3, 4)
+print('=' * 60)
+print(a)
+b = a > 4
+print('=' * 60)
+print(b)
+print('=' * 60)
+print(a[b])
+a[b] = 0
+print('=' * 60)
+print(a)
+
+
+def mandelbrot(h, w, maxit=20):
+    """Returns an image of the Mandelbrot fractal of size (h, w)."""
+    y, x = np.ogrid[-1.4:1.4:h * 1j, -2:0.8:w * 1j]
+    c = x + y * 1j
+    z = c
+    divtime = maxit + np.zeros(z.shape, dtype=int)
+
+    for i in range(maxit):
+        z = z**2 + c
+        diverge = z * np.conj(z) > 2**2
+        div_now = diverge & (divtime == maxit)
+        divtime[div_now] = i
+        z[diverge] = 2
+
+    return divtime
+
+
+plt.imshow(mandelbrot(400, 400))
+plt.show()
+
+a = np.arange(12).reshape(3, 4)
+b1 = np.array([False, True, True])
+b2 = np.array([True, False, True, False])
+print('=' * 60)
+print(a)
+print('=' * 60)
+print(a[b1, :])
+print('=' * 60)
+print(a[:, b2])
+print('=' * 60)
+print(a[b1, b2])
+
+# Linear Algebra
