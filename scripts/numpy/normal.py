8506d85f11888a0f22a4e80d35c2989e0709318b
zhongchun <zhongchunyu@gmail.com>
2018-08-17 23:50:43 +0800

    add a normal distribution

--- scripts/numpy/normal.py
+++ scripts/numpy/normal.py
@@ -19,7 +19,7 @@ plt.hist(u, 80, facecolor='g', alpha=0.75)
 plt.grid(True)
 plt.show()
 
-times = 10000
+times = 1000
 for time in range(times):
     u += np.random.uniform(0.0, 1.0, 10000)
 print(len(u))
