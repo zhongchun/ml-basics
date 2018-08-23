a410fe4d4550eca138f10331e0a1bc7b8b0c010f
zhongchun <zhongchunyu@gmail.com>
2018-08-23 23:02:33 +0800

    add matplotlib scripts

---
+++ scripts/matplotlib/line_plot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ @FileName: undefined
+ @Desc:
+ @Author: yuzhongchun
+ @Date: 2018-08-23 22:57:13
+ @Last Modified by: yuzhongchun
+ @Last Modified time: 2018-08-23 22:57:13
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+# print(t)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(
+    xlabel='time(s)',
+    ylabel='voltage(mV)',
+    title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
